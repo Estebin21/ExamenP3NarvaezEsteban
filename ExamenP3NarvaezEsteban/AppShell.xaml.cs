c548a9727a2a79757993703921386d549f318126
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2025-01-20 14:20:00 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP3NarvaezEsteban/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenP3NarvaezEsteban
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

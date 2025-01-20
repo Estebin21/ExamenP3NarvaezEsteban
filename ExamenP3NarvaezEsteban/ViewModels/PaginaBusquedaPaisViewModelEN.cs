0c333d42c9245f2b4af02c6e0413f4de89e8fd8f
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2025-01-20 16:13:34 -0500

    Creacion de los viewModels, y BaseViewModel

--- ExamenP3NarvaezEsteban/ViewModels/PaginaBusquedaPaisViewModelEN.cs
+++ ExamenP3NarvaezEsteban/ViewModels/PaginaBusquedaPaisViewModelEN.cs
@@ -1,12 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SQLite;
+using System.Collections.ObjectModel;
+using ExamenP3NarvaezEsteban.Models;
 
 namespace ExamenP3NarvaezEsteban.ViewModels
 {
-    internal class PaginaBusquedaPaisViewModelEN
+    public class PaginaBusquedaPaisViewModelEN : BaseViewModel
     {
+        public event Action PaisGuardado;
+
+        private string _nombrePais;
+        public string NombrePais
+        {
+            get => _nombrePais;
+            set => SetProperty(ref _nombrePais, value);
+        }
+
+        private string _mensajeResultado;
+        public string MensajeResultado
+        {
+            get => _mensajeResultado;
+            set => SetProperty(ref _mensajeResultado, value);
+        }
+
+        public Command BuscarPaisCommand { get; }
+        public Command LimpiarCommand { get; }
+
+        public PaginaBusquedaPaisViewModelEN()
+        {
+            BuscarPaisCommand = new Command(async () => await BuscarPais());
+            LimpiarCommand = new Command(Limpiar);
+        }
+
+        private async Task BuscarPais()
+        {
+            if (string.IsNullOrWhiteSpace(NombrePais))
+            {
+                MensajeResultado = "Por favor ingrese un nombre de país.";
+                return;
+            }
+
+            var url = $"https://restcountries.com/v3.1/name/{NombrePais}?fields=name,region,maps";
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetStringAsync(url);
+                    var paises = JsonConvert.DeserializeObject<List<PaisEN>>(response);
+                    if (paises != null && paises.Count > 0)
+                    {
+                        var primerPais = paises.First();
+                        GuardarPaisEnBD(primerPais);
+                        MensajeResultado = $"Nombre oficial del país: {primerPais.Name.Official}\n- Región: {primerPais.Region}\n- Link de Google Maps: {primerPais.Maps.GoogleMaps}\n- Nombre: ENarvaez";
+                        PaisGuardado?.Invoke();  // Dispara el evento cuando se guarda un país
+                    }
+                    else
+                    {
+                        MensajeResultado = "No se encontró ningún país con ese nombre.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MensajeResultado = $"Error al buscar el país: {ex.Message}";
+            }
+        }
+
+        private void GuardarPaisEnBD(PaisEN pais)
+        {
+            using (var conexion = new SQLiteConnection(App.DatabasePath))
+            {
+                conexion.CreateTable<PaisApiEN>();
+                var paisDB = new PaisApiEN
+                {
+                    Nombre = pais.Name.Official,
+                    Region = pais.Region,
+                    EnlaceGoogleMaps = pais.Maps.GoogleMaps,
+                    NombreUsuario = "ENarvaez" // Cambiar por tu nombre de usuario
+                };
+                conexion.Insert(paisDB);
+            }
+        }
+
+        private void Limpiar()
+        {
+            NombrePais = string.Empty;
+            MensajeResultado = string.Empty;
+        }
     }
 }
--- ExamenP3NarvaezEsteban/ViewModels/PaginaListaPaisesViewModelEN.cs
+++ ExamenP3NarvaezEsteban/ViewModels/PaginaListaPaisesViewModelEN.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using SQLite;
+using ExamenP3NarvaezEsteban.Models;
 
 namespace ExamenP3NarvaezEsteban.ViewModels
 {
-    internal class PaginaListaPaisesViewModelEN
+    public class PaginaListaPaisesViewModelEN : BaseViewModel
     {
+        public ObservableCollection<string> Paises { get; set; }
+
+        public PaginaListaPaisesViewModelEN()
+        {
+            Paises = new ObservableCollection<string>();
+            CargarPaises();
+        }
+
+        public void CargarPaises()
+        {
+            Paises.Clear();
+            using (var conexion = new SQLiteConnection(App.DatabasePath))
+            {
+                conexion.CreateTable<PaisApiEN>();
+                var paisesBD = conexion.Table<PaisApiEN>().ToList();
+                foreach (var pais in paisesBD)
+                {
+                    Paises.Add($"Nombre oficial del país: {pais.Nombre}\n- Región: {pais.Region}\n- Link de Google Maps: {pais.EnlaceGoogleMaps}\n- Nombre: {pais.NombreUsuario}");
+                }
+            }
+        }
     }
 }

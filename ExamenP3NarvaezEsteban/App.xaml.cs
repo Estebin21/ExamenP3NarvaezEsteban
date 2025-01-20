4df1d5c4c7884cbcd34654018868716ce4b8ab09
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2025-01-20 16:55:57 -0500

    Conexion a sql, correcion de errores finales y finalizacion del programa

--- ExamenP3NarvaezEsteban/App.xaml.cs
+++ ExamenP3NarvaezEsteban/App.xaml.cs
@@ -1,12 +1,23 @@
-﻿namespace ExamenP3NarvaezEsteban
+﻿using ExamenP3NarvaezEsteban.Views;
+
+namespace ExamenP3NarvaezEsteban
 {
     public partial class App : Application
     {
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, "ExamenEN.db3");
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new PaginaPrincipalEN();
         }
     }
 }

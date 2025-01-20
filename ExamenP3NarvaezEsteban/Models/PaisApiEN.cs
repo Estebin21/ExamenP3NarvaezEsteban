cfff3704e31cbd5431d2b60f414b01c65ceda1c4
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2025-01-20 15:59:17 -0500

    Creación de modelos

--- ExamenP3NarvaezEsteban/Models/PaisApiEN.cs
+++ ExamenP3NarvaezEsteban/Models/PaisApiEN.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace ExamenP3NarvaezEsteban.Models
 {
-    internal class PaisApiEN
+    public class PaisApiEN
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Region { get; set; }
+        public string EnlaceGoogleMaps { get; set; }
+        public string NombreUsuario { get; set; } 
     }
 }
--- ExamenP3NarvaezEsteban/Models/PaisEN.cs
+++ ExamenP3NarvaezEsteban/Models/PaisEN.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExamenP3NarvaezEsteban.Models
+﻿namespace ExamenP3NarvaezEsteban.Models
 {
-    internal class PaisEN
+    public class PaisEN
     {
+        public Name Name { get; set; }
+        public string Region { get; set; }
+        public Maps Maps { get; set; }
+    }
+
+    public class Name
+    {
+        public string Official { get; set; }
+    }
+
+    public class Maps
+    {
+        public string GoogleMaps { get; set; }
     }
 }
